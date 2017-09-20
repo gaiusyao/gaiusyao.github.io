f90c4be99a77918f528eedbb13f4d7e3e824676e
gaiusyao <gaiusyao@yeah.net>
2017-09-20 14:55:13 +0800

    update cssimg

--- js/pivotlab/csslab.js
+++ js/pivotlab/csslab.js
@@ -1,19 +1,43 @@
 /**
+ * PivotLab
+ * CSSLab
+ * Created by @Gaius42
+ */
+
+
+/**
+ * CSS image
+ */
+var cssFilter = function() {
+	$("#css-img").css("filter", "grayscale("+grayscale.getValue()+"%"+")")
+}; // cssFilter
+
+/* grayscale */
+var grayscale = $("#ex6").slider()
+				.on("slide", cssFilter)
+				.data("slider");
+
+$("#ex6").slider();
+$("#ex6").on("slide", function(slideEvt) {
+    $("#ex6SliderVal").text(slideEvt.value);
+}); 
+
+/**
  * RGB Slider
  */
 
-var RGBaChange = function() {
+var RGBChange = function() {
 	$(".bg-blend").css("background-color", "rgb("+r.getValue()+","+g.getValue()+","+b.getValue()+")")
 }; // bg-color
 
 var r = $("#R").slider()
-		.on("slide", RGBaChange)
+		.on("slide", RGBChange)
 		.data("slider");
 var g = $("#G").slider()
-		.on("slide", RGBaChange)
+		.on("slide", RGBChange)
 		.data("slider");
 var b = $("#B").slider()
-		.on("slide", RGBaChange)
+		.on("slide", RGBChange)
         .data("slider");
 
 $("#R").slider();
