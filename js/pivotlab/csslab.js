2e862d7eb65112ede6df1d6397eb8583dbad5373
gaiusyao <gaiusyao@yeah.net>
2017-09-22 18:13:07 +0800

    update mdcolor

--- js/pivotlab/csslab.js
+++ js/pivotlab/csslab.js
@@ -102,37 +102,3 @@ var blur = $("#blur").slider()
 $("#blur").on("slide", function (slideEvt) {
 	$("#blurVal").text(slideEvt.value);
 }); 
-
-
-/**
- * RGB Slider
- */
-
-var RGBChange = function() {
-	$(".bg-blend").css("background-color", "rgb("+r.getValue()+","+g.getValue()+","+b.getValue()+")")
-}; // bg-color
-
-var r = $("#R").slider()
-		.on("slide", RGBChange)
-		.data("slider");
-var g = $("#G").slider()
-		.on("slide", RGBChange)
-		.data("slider");
-var b = $("#B").slider()
-		.on("slide", RGBChange)
-        .data("slider");
-
-$("#R").slider();
-$("#R").on("slide", function(slideEvt) {
-    $("#rSliderVal").text(slideEvt.value);
-}); //rSilderVal
-
-$("#G").slider();
-$("#G").on("slide", function(slideEvt) {
-    $("#gSliderVal").text(slideEvt.value);
-}); //gSilderVal
-
-$("#B").slider();
-$("#B").on("slide", function(slideEvt) {
-    $("#bSliderVal").text(slideEvt.value);
-}); //bSilderVal
